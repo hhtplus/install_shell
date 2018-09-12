ef81990fecab21eff909599215f581961c7c71ec
hanhaitao <hhtplus@gmail.com>
2018-09-12 09:44:19 +0800

    Update update_php_to_56.sh

--- update_php_to_56.sh
+++ update_php_to_56.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-https://webtatic.com/packages/php56/
+#https://webtatic.com/packages/php56/
+
+yum install epel-release -y
+
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
+yum install php56w php56w-opcache php56w-mysql  php56w-gd  -y
